1dc790690108e64cd9e1786eec491bad10dfb4b5
Vadim <kv3@list.ru>
2022-07-20 22:41:05 +0300

    added timeZone and SSL

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,7 +8,7 @@ public class Util {
     // реализуйте настройку соеденения с БД
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "root1";
-    private static final String URL = "jdbc:MySQL://localhost/pptest";
+    private static final String URL = "jdbc:MySQL://localhost/pptest?serverTimezone=Europe/Moscow&useSSL=false";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
     public static Connection getConnection(){
